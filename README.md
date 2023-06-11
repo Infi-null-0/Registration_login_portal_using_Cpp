2354aabc1efd4792936e3e05273a3be4b502fdfb
infi-null-0-local <119103652+Infi-null-0@users.noreply.github.com>
2023-06-11 16:40:18 +0530

    commit done !

---
+++ README.md
@@ -0,0 +1,63 @@
+# Login And Resgister
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+> Registeration and login page !
+
+## Getting Started
+
+To get started with the program, follow these steps:
+
+1. Clone the repository to your local machine:
+
+    ```shell
+    git clone https://github.com/Infi-null-0/Registration_login_portal_using_Cpp.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```shell
+    cd Registration_login_potal_using_cpp
+    ```
+
+3. Compile the source code using a C++ compiler
+
+4. Run the executable:
+
+## Contributing
+
+Contributions are always welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+Fork the repository.
+
+1. Fork The Repository
+
+2. Create your branch:
+
+    ```shell
+    git checkout -b feature/your-feature
+    ```
+
+3. Commit your changes:
+
+    ```shell
+    git commit -am 'Add some feature'
+    ```
+
+4. Push to the branch:
+
+    ```shell
+    git push origin feature/your-feature
+    ```
+
+5. Open a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## INFO
+
+    This project is just a prototype !
+
+    It is still under development !
---
+++ log_and_reg.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <limits>
+
+// console initialization
+void console_init()
+{
+    system("cls");
+    std::cout << "\t--- REGISTRATION AND LOGIN - PORTAL ---\n";
+}
+
+//// this works most of the time to clear your cin buffer
+//  flush_cin_buffer
+void flush()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+// this is the database file!
+std::fstream file("Database.txt", std::ios::in | std::ios::app);
+
+void page_reg()
+{
+    std::string username, password, confirm;
+    std::string line;
+    console_init();
+
+    // registration portal
+    std::cout << "\n\t       -- REGISTRATION PORTAL -- \n";
+
+    // uname
+    std::cout << "\n\t\tUsername : ";
+    std::cin >> username;
+    flush();
+
+    // check if username exists
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::string storedUsername;
+        iss >> storedUsername;
+        if (storedUsername == username)
+        {
+            std::cout << "\n\t     -- USERNAME ALREADY EXISTS -- \n";
+            std::cout << "\t           Try again? (y): ";
+            char choice = 'n';
+            std::cin >> choice;
+            if (choice == 'y')
+            {
+                page_reg();
+                return;
+            }
+        }
+    }
+    file.clear();
+
+    // pass
+    std::cout << "\t\tPassword : ";
+    std::cin >> password;
+    flush();
+
+    // confirm pass
+    std::cout << "\n\t\tConfirm \n\t\tPassword : ";
+    std::cin >> confirm;
+    flush();
+
+    // if passwords dont match
+    if (password != confirm)
+    {
+        std::cout << "\n\n\t     -- PASSWORD DOES NOT MATCH -- \n";
+        std::cout << "\n\n\t           Try again? (y): ";
+        char choice = 'n';
+        std::cin >> choice;
+        if (choice == 'y')
+        {
+            page_reg();
+            return;
+        }
+        return;
+    }
+
+    // write to file
+    file << username << " " << password << "\n";
+    file.flush();
+    file.clear();
+
+    // success
+    std::cout << "\n\t     -- REGISTRATION SUCCESSFUL -- \n";
+
+    return;
+}
+
+void page_log()
+{
+    std::string username, password, line;
+    bool flag = false;
+    console_init();
+
+    // login portal
+    std::cout << "\n\t\t  -- LOGIN PORTAL -- \n";
+
+    // uname
+    std::cout << "\n\t\tUsername : ";
+    std::cin >> username;
+    flush();
+
+    // check if username exists
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::string storedUsername;
+        iss >> storedUsername;
+        if (storedUsername == username)
+        {
+            flag = true;
+            break;
+        }
+    }
+    if (flag = false)
+    {
+        std::cout << "\n\t     -- USERNAME DOES NOT EXIST -- \n";
+        std::cout << "\t           Try again? (y): ";
+        char choice = 'n';
+        std::cin >> choice;
+        if (choice == 'y')
+        {
+            page_log();
+            return;
+        }
+    }
+    file.clear();
+
+    // pass
+    std::cout << "\t\tPassword : ";
+    std::cin >> password;
+    flush();
+
+    // check if password matches
+    std::istringstream iss(line);
+    std::string storedUsername, storedPassword;
+    iss >> storedUsername >> storedPassword;
+    if (password != storedPassword)
+    {
+        std::cout << "\n\n\t       -- INCORRECT PASSWORD -- \n";
+        std::cout << "\t           Try again? (y): ";
+        char choice = 'n';
+        std::cin >> choice;
+        if (choice == 'y')
+        {
+            page_log();
+            return;
+        }
+    }
+    file.clear();
+
+    std::cout << "\n\t        -- LOGIN SUCCESSFUL -- \n";
+
+    return;
+}
+
+void init_page()
+{
+    console_init();
+    std::cout << "\n\t\t   Choose an option \n\t\t1- Register \n\t\t2- Login \n\t\t3- Exit \n";
+    std::cout << "\n\t\t   Your choice : ";
+    ////std::cout << "\n\n\t    ";
+    ////system("pause");
+
+    short int choice = 1;
+    std::cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        page_reg();
+        break;
+    case 2:
+        page_log();
+        break;
+    case 3:
+        std::cout << "\n\t      -- Program Terminated -- \n";
+        exit(1);
+    default:
+        std::cout << "\n\t\t-- Invalid Choice -- \n";
+        std::cout << "\t      -- Program terminated -- \n";
+        exit(1);
+    }
+}
+
+int main()
+{
+    if (!file.is_open())
+    {
+        std::cout << "ERROR !\n\t * File not found !\n\t * Link file properly and run again !" << std::endl;
+        return 0;
+    }
+
+    // page1()
+    init_page();
+}
